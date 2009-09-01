afadaee7d754de91137ffbfb343109aa0fe2ccfd
Nick Zadrozny <nick@beyondthepath.com>
2009-08-31 19:50:59 -0700

    Show a more interesting title, and fix the color span for non-ajax search results.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,5 +4,17 @@ module ApplicationHelper
   def logged_in?
     current_person.present?
   end
+  
+  def t(str)
+    @title = str
+  end
+  
+  def title
+    [@title, site_title].compact.join(" -  ")
+  end
+  
+  def site_title
+    "MissionsBridge"
+  end
 
 end
