bf0afe4d3b6907c87091d0de7c37e9c3c5b64be4
Nick Zadrozny <nick@beyondthepath.com>
2009-10-01 00:05:34 -0700

    Add a coming soon message, and move a config value for heroku

--- config/initializers/load_yahoo_config.rb
+++ config/initializers/load_yahoo_config.rb
@@ -1,8 +1,13 @@
 require 'yaml'
-begin
-  YAHOO = YAML.load_file("#{RAILS_ROOT}/config/yahoo.yml")
-  raise if YAHOO['appid'] == nil
-rescue
-  puts "Couldn't find a Yahoo! Application ID in config/yahoo.yml. See config/yahoo.yml.example"
-  exit
+if RAILS_ENV == "production"
+  YAHOO = {}
+  YAHOO['appid'] = ENV["YAHOO_APPID"]
+else
+  begin
+    YAHOO = YAML.load_file("#{RAILS_ROOT}/config/yahoo.yml")
+    raise if YAHOO['appid'] == nil
+  rescue
+    puts "Couldn't find a Yahoo! Application ID in config/yahoo.yml. See config/yahoo.yml.example"
+    exit
+  end  
 end
