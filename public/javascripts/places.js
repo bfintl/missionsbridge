fde9c95f2c20b3ff8a72250889f4af10c9d5b8e2
Nick Zadrozny <nick@beyondthepath.com>
2009-09-30 21:00:40 -0700

    Move coordinate translation functions to places.js

--- public/javascripts/places.js
+++ public/javascripts/places.js
@@ -59,3 +59,48 @@
   };
   
 })(jQuery);
+
+var sampleLatArray = new Array();
+var sampleLonArray = new Array();
+var sampleXArray = new Array();
+var sampleYArray = new Array();
+
+var pushSampleCoord = function(lat, lon, x, y) {
+  sampleLatArray.push(lat);
+  sampleLonArray.push(lon);
+  sampleXArray.push(x);
+  sampleYArray.push(y);
+};
+
+var calculateWeights = function(a, samples) {
+  var weights = new Array();
+  for (var i = 0; i < samples.length; i++) {
+    weights[i] = 1.0 / Math.abs(a - samples[i])
+  }
+  return weights;
+};
+
+var weightedMean = function(values, weights) {
+  var valueSum  = 0;
+  var weightSum = 0;
+  for (var i = 0; i < values.length; i++) {
+    if (weights[i] == Infinity) {
+      return values[i];
+    } else {
+      valueSum += values[i] * weights[i];
+      weightSum += weights[i];
+    }
+  }
+  return valueSum / weightSum;
+}
+
+var latLonToXY = function(lat, lon) {
+  // 1. calculate weights
+  var latWeights = calculateWeights(lat, sampleLatArray)
+  var lonWeights = calculateWeights(lat, sampleLonArray)
+  // 2. calculate weighted means
+  var x = weightedMean(sampleXArray, lonWeights);
+  var y = weightedMean(sampleYArray, latWeights);
+  // 3. done!
+  return [Math.round(x), Math.round(y)];
+};
