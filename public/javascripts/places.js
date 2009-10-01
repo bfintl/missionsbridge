8a1597e4896b62dc374f936fa53c50fa21df2346
Nick Zadrozny <nick@beyondthepath.com>
2009-09-30 22:02:39 -0700

    Debugging the placement function, adding more place coordinates.

--- public/javascripts/places.js
+++ public/javascripts/places.js
@@ -1,6 +1,8 @@
 (function($){
 
-  $.fn.searchPlaces = function() {
+  $.fn.searchPlaces = function(options) {
+    
+    if (!options) { options = {} };
 
     var searchField = this;
     var resultsField = $("#" + this.attr("id").replace("search", "results"));
@@ -9,9 +11,9 @@
     var timeoutDelay = 400;
     var throttledDelay = 1000;
     var throttleFactor = 1.5;
+    var callback = options.callback;
 
     var updatePlacesList = function(data) {
-      window.console.log("Updating place list...");
       var placesList = $("<ul class='places'></ul>");
       $.each(data, function(i, item) {
         var place = item.place;
@@ -19,31 +21,35 @@
         var placeColor = "<span style='background-color:#" + place.color + "'>&nbsp;</span>";
         var placeLink = "<a href='/places/" + place.permalink + "'>" +  placeName + "</a>";
         var dataCentroid = "data:centroid-lat='" + place.centroid_lat + "' data:centroid-lon='" + place.centroid_lon + "'";
-        var placeLi = "<li class='place' " + dataCentroid + ">" + placeColor + " " + placeLink + "</li>";
+        var placeLi = "<li class='place' " + dataCentroid + "><span>" + placeColor + " " + placeLink + "</span></li>";
         $(placeLi).appendTo(placesList);
       });
       resultsField.html(placesList);
+      if (callback) { callback(); }
     };
 
     var performSearch = function() {
-      resultsField.html("<div class='indicator'>Please wait&hellip;</div>");
-
       var searchQuery = searchField.val();
       var searchUrl = "/places/search?q=" + searchQuery;
 
-      $.getJSON(searchUrl, function(data) {
-        window.console.log(data);
-        if (data && data.length > 0) {
-          throttledDelay = 1000;
-          updatePlacesList(data);
-        } else if (throttledDelay * throttleFactor < 3000) {
-          throttledDelay = throttledDelay * throttleFactor;
-          triggerSearch(throttledDelay);
-        } else {
-          throttledDelay = 1000;
-          resultsField.html("<div class='indicator'>No results found.</div>");
-        }
-      });
+      if (searchQuery && searchQuery != "") {
+        resultsField.html("<div class='indicator'>Please wait&hellip;</div>");
+
+        $.getJSON(searchUrl, function(data) {
+          if (data && data.length > 0) {
+            throttledDelay = 1000;
+            updatePlacesList(data);
+          } else if (throttledDelay * throttleFactor < 3000) {
+            throttledDelay = throttledDelay * throttleFactor;
+            triggerSearch(throttledDelay);
+          } else {
+            throttledDelay = 1000;
+            resultsField.html("<div class='indicator'>No results found.</div>");
+          }
+        });
+      } else {
+        resultsField.html("<div class='indicator'></div>");
+      }
     };
 
     var triggerSearch = function(extraTimeoutDelay) {
@@ -66,6 +72,7 @@ var sampleXArray = new Array();
 var sampleYArray = new Array();
 
 var pushSampleCoord = function(lat, lon, x, y) {
+  window.console.log([lat, lon] + " => " + [x, y]);
   sampleLatArray.push(lat);
   sampleLonArray.push(lon);
   sampleXArray.push(x);
@@ -97,10 +104,28 @@ var weightedMean = function(values, weights) {
 var latLonToXY = function(lat, lon) {
   // 1. calculate weights
   var latWeights = calculateWeights(lat, sampleLatArray)
-  var lonWeights = calculateWeights(lat, sampleLonArray)
+  var lonWeights = calculateWeights(lon, sampleLonArray)
   // 2. calculate weighted means
   var x = weightedMean(sampleXArray, lonWeights);
   var y = weightedMean(sampleYArray, latWeights);
   // 3. done!
   return [Math.round(x), Math.round(y)];
 };
+
+var setPlacesOnMap = function() {
+  $(".place").each(function(i){
+    var coords = latLonToXY($(this).attr('data:centroid-lat'), $(this).attr('data:centroid-lon'));
+    var offset = $("#world-map").offset();
+    var placeLeft = coords[0] + offset.left;
+    var placeTop = coords[1] + offset.top;
+    var placeWidth = $(this).css("width");
+    var placeStyle = {
+      position: 'absolute',
+      left: placeLeft, top: placeTop,
+      overflow: 'visible',
+      width: placeWidth
+    };
+    window.console.log([placeLeft, placeTop]);
+    $(this).css(placeStyle);
+  });
+}
